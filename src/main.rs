ab136fcab57f9282525b3be7bb6ae5be0be0619c
Daniel Javorszky <daniel.javorszky@gmail.com>
2023-12-07 18:46:38 +0100

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,25 @@ mod day5;
 mod day6;
 mod prelude;
 
+use clap::Parser;
 use prelude::*;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    day: u8,
+}
+
 fn main() -> Result<()> {
-    day6::run_day()?;
+    let cli = Cli::parse();
+
+    println!("Running Day {}", cli.day);
+    match cli.day {
+        5 => day5::run_day()?,
+        6 => day6::run_day()?,
+        _ => println!("Day {} not yet implemented", cli.day),
+    }
 
     Ok(())
 }
