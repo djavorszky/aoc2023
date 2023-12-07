85a938c94924e7592a8557a8880f96256cf0a4a7
Daniel Javorszky <daniel.javorszky@gmail.com>
2023-12-07 15:52:27 +0100

    day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,11 +1,38 @@
-use std::{ops::RangeInclusive, str::FromStr};
+use std::str::FromStr;
+
+use itertools::Itertools;
 
 use crate::prelude::*;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+struct Range {
+    start: i128,
+    end: i128,
+}
+
+impl Range {
+    fn new(start: i128, end: i128) -> Self {
+        Range { start, end }
+    }
+
+    fn overlaps(&self, other: &Range) -> bool {
+        self.start < other.end && other.start < self.end
+    }
+
+    fn shift(&self, offset: i128) -> Range {
+        Range::new(self.start + offset, self.end + offset)
+    }
+
+    fn contains(&self, n: i128) -> bool {
+        self.start <= n && n <= self.end
+    }
+}
 
 pub fn run_day() -> Result<()> {
     let input = include_str!("../input/5.txt");
 
     println!("task 1: {}", task1(input)?);
+    println!("task 2: {}", task2(input)?);
 
     Ok(())
 }
@@ -40,18 +67,39 @@ fn transform(n: i128, transformations: &[Transformation]) -> i128 {
 }
 
 fn task2(input: &str) -> Result<i128> {
-    Ok(0)
+    let input = input.parse::<Task2Input>()?;
+
+    let seeds = input.seeds;
+    let mappers = input.mappers;
+
+    let mut result = Vec::new();
+
+    for seed_range in seeds {
+        let mut ranges = vec![seed_range];
+
+        for block in &mappers {
+            ranges = Vec::from_iter(
+                ranges
+                    .into_iter()
+                    .flat_map(|r| apply_transformations(r, block.clone())),
+            );
+        }
+
+        result.push(ranges.iter().min_by_key(|r| r.start).unwrap().start);
+    }
+
+    Ok(*result.iter().min().unwrap())
 }
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Clone)]
 struct Transformation {
-    range: RangeInclusive<i128>,
+    range: Range,
     op: i128,
 }
 
 impl Transformation {
     fn in_range(&self, n: i128) -> bool {
-        self.range.contains(&n)
+        self.range.contains(n)
     }
 }
 
@@ -66,9 +114,52 @@ impl FromStr for Transformation {
         let size = parts.next().map(|c| c.parse::<i128>()).unwrap()?;
 
         Ok(Self {
-            range: (start..=start + size),
+            range: Range::new(start, start + size),
             op: end - start,
         })
+    }
+}
+
+#[derive(Debug)]
+struct Task2Input {
+    seeds: Vec<Range>,
+    mappers: Vec<Vec<Transformation>>,
+}
+
+impl FromStr for Task2Input {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
+        let mut chunks = s.split("\n\n");
+
+        let seeds = chunks
+            .next()
+            .and_then(|line| line.split_once(": "))
+            .map(|(_, nums)| {
+                let mut ranges = Vec::new();
+                for mut chunk in &nums
+                    .split_ascii_whitespace()
+                    .map(|n| n.parse::<i128>().unwrap())
+                    .chunks(2)
+                {
+                    ranges.push(Range::new(chunk.next().unwrap(), chunk.next().unwrap()))
+                }
+
+                ranges
+            })
+            .unwrap();
+
+        let mappers = chunks
+            .map(|chunk| {
+                chunk
+                    .lines()
+                    .skip(1)
+                    .map(|line| line.parse::<Transformation>().unwrap())
+                    .collect()
+            })
+            .collect();
+
+        Ok(Self { seeds, mappers })
     }
 }
 
@@ -108,10 +199,90 @@ impl FromStr for Task1Input {
     }
 }
 
+fn apply_transformations(base: Range, transforms: Vec<Transformation>) -> Vec<Range> {
+    let mut result = Vec::new();
+
+    for transformation in transforms.clone() {
+        let mask = transformation.range;
+        let offset = transformation.op;
+
+        // 1 - no overlap; skip this mask
+        if !base.overlaps(&mask) {
+            continue;
+        }
+
+        // 2 - base is inside mask, shift entire base
+        if mask.start <= base.start && base.end <= mask.end {
+            return vec![base.shift(offset)];
+        }
+
+        // 3 - mask is a subset of base
+        // return unshifted left, shifted middle, and recurse for the rest
+        if base.start <= mask.start && mask.end <= base.end {
+            result.push(Range {
+                start: base.start,
+                end: mask.start,
+            });
+            result.push(mask.shift(offset));
+            result.extend(apply_transformations(
+                Range {
+                    start: mask.end,
+                    end: base.end,
+                },
+                transforms.clone(),
+            ));
+            return result;
+        }
+
+        // 4 - mask overlaps only the left side,
+        // return masked left, recurse for the rest
+        if mask.start <= base.start && mask.end <= base.end {
+            result.push(Range {
+                start: base.start + offset,
+                end: mask.end + offset,
+            });
+            result.extend(apply_transformations(
+                Range {
+                    start: mask.end,
+                    end: base.end,
+                },
+                transforms.clone(),
+            ));
+            return result;
+        }
+
+        // 5 - mask overlaps only the right side
+        // return unshifted left, masked right
+        if base.start <= mask.start && base.end <= mask.end {
+            result.push(Range {
+                start: base.start,
+                end: mask.start,
+            });
+            result.push(Range {
+                start: mask.start + offset,
+                end: base.end + offset,
+            });
+            return result;
+        }
+    }
+
+    // no masks overlapped this base; pass it through
+    result.push(base);
+    result
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
+
+    #[test]
+    fn parse_task2() {
+        let parsed = TEST.parse::<Task2Input>().unwrap();
+
+        assert_eq!(parsed.seeds.len(), 2);
+        assert_eq!(parsed.mappers.len(), 7);
+    }
 
     #[test]
     fn test_task1() {
@@ -141,7 +312,7 @@ mod tests {
             "50 98 2".parse::<Transformation>().unwrap(),
             Transformation {
                 op: -48,
-                range: (98..=100)
+                range: Range::new(98, 100)
             }
         )
     }
